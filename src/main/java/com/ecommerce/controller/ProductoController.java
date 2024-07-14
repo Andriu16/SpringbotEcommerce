81ed16340afcc7b7700bfe120beff6c2210612a5
Andriu VALLE HUAMANI <lokitoztony@gmail.com>
2024-07-13 19:59:38 -0500

    sig3

---
+++ src/main/java/com/ecommerce/controller/ProductoController.java
@@ -0,0 +1,49 @@
+package com.ecommerce.controller;
+
+import java.util.Optional;
+
+
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ecommerce.model.Producto;
+import com.ecommerce.model.Usuario;
+
+import com.ecommerce.service.ProductoService;
+
+
+@Controller
+@RequestMapping("/productos")
+public class ProductoController {
+	
+	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+	
+	@Autowired
+	private ProductoService productoService;
+	
+	
+	
+	
+	@GetMapping("")
+	public String show() {
+		return "productos/show";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
